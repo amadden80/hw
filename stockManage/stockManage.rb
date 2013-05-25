4b7b6991ab35f7672053d21c12f2632d9b2bebcb
amadden80 <amadden80@gmail.com>
2013-05-25 18:26:24 -0400

    Stock Push Try

--- stockManage/stockManage.rb
+++ stockManage/stockManage.rb
@@ -1,4 +1,162 @@
 
+
+require 'YahooFinance'
+
+
 class StockManagement
+
+  attr_reader :name, :clients
+
+  def initialize(name)
+    @name = name
+    @clients = []
+  end
+
+  def newClient(name, cash)
+    client = Client.new(name)
+    client.cash = cash
+    @clients << client
+  end
+
+  def getClient(name)
+    @clients.each{|cur| cur.name == name}.first
+  end
+
+
+  def to_s
+    printString = "#{@name}\n"
+    @clients.each{|cur| printString << "#{cur.name}: #{cur.balance}\n"}
+    return printString
+  end
+
+  def displayClients
+    puts @clients.map{|cur| "#{cur.name}"}.join(', ')
+  end
+
+end
+
+
+class Client
+
+  attr_accessor :name, :cash, :portfolios
+
+  def initialize(name)
+    @name = name
+    @balance = nil
+    @portfolios = []
+  end
+
+  def newPortfolio(name, id, numShares)
+    if Stock.new(id).price * numShares < @cash
+      portfolio = Portfolio.new(name)
+      cost = portfolio.addStock(id, numShares)
+      @cash -= cost
+      @portfolios << portfolio
+    else
+      puts "Purchace price exceeds cash.  Transaction Ignored"
+    end
+  end
+
+  def buyStock(portfolioName, id, numShares)
+    portfolio = @portfolios.each {|port| port.name == portfolioName}.first
+
+    if Stock.new(id).price * numShares < @cash
+      cost = portfolio.addStock(id, numShares)
+      @cash -= cost
+    else
+      puts "Purchace price exceeds cash.  Transaction Ignored"
+    end
+
+  end
+
+  def sellStock(portfolioName, id, numShares)
+    portfolio = @portfolios.each {|port| port.name == portfolioName}.first
+    cost = portfolio.removeStock(id, numShares)
+    @cash += cost
+  end
+
+  def update
+    @portfolios.each do |portfolio|
+      portBalance = 0
+      portfolio.stocks.each do |stock|
+        stock[:value] = stock[:numShares] * (stock[:stock].update)
+        portBalance += stock[:value]
+      end
+      portfolio.balance = portBalance
+    end
+  end
+
+  def balance
+    update
+    balance = 0
+    @portfolios.each{|portfolio| balance+=portfolio.balance}
+    return balance
+  end
+
+  def to_s
+    printString = "\n#{@name} \n"
+    @portfolios.each { |portfolio|  printString << "#{portfolio.name} : #{portfolio.name}\n"}
+    return printString
+  end
+
+end
+
+
+class Portfolio
+
+  attr_accessor :name, :stocks, :balance
+
+  def initialize(name)
+    @name = name
+    @stocks = []
+    @balance = nil
+  end
+
+  def addStock(id, numShares)
+    stock = Stock.new(id)
+    cost = stock.price * numShares
+    stocks << {:stock => stock, :numShares => numShares, :cost => cost, :value => cost}
+    return cost
+  end
+
+
+  def removeStock(id, numShares)
+
+    stock = @stocks.each {|stk| stk[:stock].id == id}.first
+
+    stock[:stock].update
+    cost = stock[:value] * numShares
+
+    if numShares < stock[:numShares]
+      stock[:numShares] -= numShares
+    elsif numShares == stock[:numShares]
+      @stocks.pop(stock)
+    end
+
+    return cost
+  end
+
+  def to_s
+    printString = "\n#{@name} \n"
+    @stocks.each { |stock|  printString << "#{stock[:stock].id} : #{stock[:numShares]} shares = totaling $#{stock[:value]}\n"}
+    return printString
+  end
+
+end
+
+
+class Stock
+
+  attr_accessor :id, :price
+
+  def initialize(id)
+    @id = id
+    @price = YahooFinance::get_quotes(YahooFinance::StandardQuote, @id)[@id].lastTrade
+  end
+
+  def update
+    @price = YahooFinance::get_quotes(YahooFinance::StandardQuote, @id)[@id].lastTrade
+    return @price
+  end
 
 end
--- stockManage/stockManage_spec.rb
+++ stockManage/stockManage_spec.rb
@@ -1,37 +1,165 @@
 
-
 require_relative 'stockManage'
-
 
 describe StockManagement do
 
   describe '#new' do
     it 'builds a new StockManagement with a name' do
-      stockManagement = StockManagement.new('MyStocky')
+      stockManagement = StockManagement.new('TDDStock')
       stockManagement.should be_instance_of StockManagement
-      stockManagement.name.should == 'MyStocky'
+      stockManagement.name.should == 'TDDStock'
+    end
+  end
+
+  describe '#new client' do
+    it 'Create an account for client (name, balance)' do
+      stockManagement = StockManagement.new('MrStockyManager')
+      name = "Joe"
+      balance = 500
+      stockManagement.newClient(name, balance)
+      stockManagement.getClient(name).name.should == name
     end
   end
 
 
+  describe '#new portfolios' do
 
-  it "Create an account for client (name, balance)"
-  it "A client can create multiple portfolios"
-  it "A client can buy stocks at market rate"
+    it "make new portfolio" do
+      stockManagement = StockManagement.new('MrStockyManager')
+      name = "Joe"
+      balance = 50000
+      id = "AAPL"
+      numShares = 1
+      portName = "MyPortfolio"
+      stockManagement.newClient(name, balance)
+      stockManagement.getClient(name).newPortfolio(portName, id, numShares)
+      stockManagement.getClient(name).portfolios[0].stocks[0][:stock].id.should == id
+    end
+
+    it "make multiple portfolio" do
+      stockManagement = StockManagement.new('MrStockyManager')
+      name = "Joe"
+      stockManagement.newClient(name, 50000)
+      stockManagement.getClient(name).newPortfolio("Port1", "AAPL", 1)
+      stockManagement.getClient(name).newPortfolio("Port2", "GE", 3)
+      stockManagement.getClient(name).portfolios.length == 2
+    end
+
+    it "buy multiple stocks in a portfolio" do
+      stockManagement = StockManagement.new('MrStockyManager')
+      name = "Joe"
+      stockManagement.newClient(name, 50000)
+      stockManagement.getClient(name).newPortfolio("Port1", "AAPL", 1)
+      stockManagement.getClient(name).portfolios[0].addStock("GE", 2)
+      stockManagement.getClient(name).portfolios[0].stocks.length.should == 2
+    end
+
+  end
+
+  describe '#exchange stocks' do
+    it "Client sells more shares" do
+      stockManagement = StockManagement.new('MrStockyManager')
+      name = "Joe"
+      startingCash = 500000
+      stockManagement.newClient(name, 5)
+      stockManagement.getClient(name).newPortfolio("Port1", "AAPL", 2)
+      stockManagement.getClient(name).sellStock("Port1", "AAPL", 1)
+      stockManagement.getClient(name).portfolios[0].stocks[0][:numShares].should == 1
+    end
+  end
+
+  describe '#exchange money' do
+    it "Client purchase subtracts from client cash" do
+      stockManagement = StockManagement.new('MrStockyManager')
+      name = "Joe"
+      startingCash = 50000
+      stockManagement.newClient(name, 5)
+      stockManagement.getClient(name).newPortfolio("Port1", "AAPL", 2)
+      stockManagement.getClient(name).cash.should < startingCash
+    end
+
+    it "Client sells more shares for $" do
+      stockManagement = StockManagement.new('MrStockyManager')
+      name = "Joe"
+      startingCash = 5000000
+      stockManagement.newClient(name, startingCash)
+      stockManagement.getClient(name).newPortfolio("Port1", "AAPL", 2)
+      cashAfterBuy = stockManagement.getClient(name).cash
+      stockManagement.getClient(name).sellStock("Port1", "AAPL", 1)
+      cashAfterSell = stockManagement.getClient(name).cash
+      cashAfterBuy.should < cashAfterSell
+    end
+    
+    it "protect for negitive cash" do
+      stockManagement = StockManagement.new('MrStockyManager')
+      name = "Joe"
+      stockManagement.newClient(name, 1)
+      stockManagement.getClient(name).newPortfolio("Port1", "AAPL", 100000000000000)
+      stockManagement.getClient(name).portfolios.length.should == 0
+    end
+
+  end
+
+  describe '#update stocks' do
+    it "stock price updated" do
+      stockManagement = StockManagement.new('MrStockyManager')
+      name = "Joe"
+      startingCash = 500
+      stockManagement.newClient(name, 500)
+      stockManagement.getClient(name).newPortfolio("Port1", "AAPL", 20)
+      bal1 = stockManagement.getClient(name).portfolios[0].balance
+      stockManagement.getClient(name).update
+      bal2 = stockManagement.getClient(name).portfolios[0].balance
+      bal1.should_not == bal2
+    end
+  end
   
-  it "Stocks will be added to a client portfolio"
-  it "Client purchase subtract from cash their clash"
-  it "Negative cash balance is not possible"
+  describe '#print lists' do
+    it "show all stocks in a portfolio" do
+      stockManagement = StockManagement.new('MrStockyManager')
+      name = "Joe"
+      startingCash = 500
+      stockManagement.newClient(name, 500)
+      stockManagement.getClient(name).newPortfolio("Port1", "AAPL", 20)
+      stockManagement.getClient(name).portfolios[0].addStock("GE", 10)
+      puts stockManagement.getClient(name).portfolios[0]
+    end
 
-  it "A client can sell a stock"
-  it "Proceeds go into clients account at sale"
+    it "show all client in a portfolio" do
+      stockManagement = StockManagement.new('MrStockyManager')
+      name = "Joe"
+      startingCash = 500
+      stockManagement.newClient(name, 500)
+      stockManagement.getClient(name).newPortfolio("Port1", "AAPL", 20)
+      stockManagement.getClient(name).portfolios[0].addStock("GE", 10)
+      puts stockManagement.getClient(name).portfolios[0]
+    end
 
-  it "List all client portfolios and their values"
-  it "List all stocks in a portfolio"
-  it "List all clients"
+    it "show clients and balance" do
+      stockManagement = StockManagement.new('MrStockyManager')
+      stockManagement.newClient("Joe", 500)
+      stockManagement.newClient("Tina", 1003)
+      stockManagement.getClient("Joe").newPortfolio("Port1", "AAPL", 2)
+      stockManagement.getClient("Tina").newPortfolio("Port1", "GE", 3)
+      stockManagement.getClient("Joes").portfolios[0].addStock("GE", 10)
+      stockManagement.getClient("Tina").newPortfolio("Port1", "AAPL", 4)
+      puts stockManagement
+    end
 
-  it "Calculate portfolio balance"
-  it "Get current stock prices"
+    it "show clients and balance" do
+      stockManagement = StockManagement.new('MrStockyManager')
+      stockManagement.newClient("Joe", 500)
+      stockManagement.newClient("Tina", 1003)
+      stockManagement.getClient("Joe").newPortfolio("Port1", "AAPL", 2)
+      stockManagement.getClient("Tina").newPortfolio("Port1", "GE", 3)
+      stockManagement.getClient("Joes").portfolios[0].addStock("GE", 10)
+      stockManagement.getClient("Tina").newPortfolio("Port1", "AAPL", 4)
+      stockManagement.displayClients
+    end
+
+  end
 
 end
+
+
 
